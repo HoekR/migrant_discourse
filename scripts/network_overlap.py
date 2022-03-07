b902d4b563d929a04d3ce87816abc19edfb1293f
marijnkoolen <marijn.koolen@gmail.com>
2022-03-07 15:22:20 +0000

    Restructure repo following JDH guidelines

---
+++ scripts/network_overlap.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import scripts.datasets as dataset_api
+from scripts.data_wrangling import map_dataset, acronym, yr2cat, highlight_decade
+from scripts.data_wrangling import parse_surname, parse_surname_initial, parse_author_index_name
+
+
